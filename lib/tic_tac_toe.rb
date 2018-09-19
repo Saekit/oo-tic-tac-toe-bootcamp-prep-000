1f0d05f4c7d87716352b9902887439b596bd04c8
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-19 20:51:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,8 +37,8 @@ def position_taken?(index)
 end
 
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
 end
 
 

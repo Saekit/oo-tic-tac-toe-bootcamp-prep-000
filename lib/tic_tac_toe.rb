da87af695f67c7c92f4c8715778f591d0baf0990
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-19 19:43:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,20 @@
 class TicTacToe
-  def initialize(board)
-    @board = [" "," "," "," "," "," "," "," "," "]
+  def initialize
+    @board = []
   end
   
-  def board=(board)
-    @board = board
+  def board
+    board = " "
+    @board << 9.times(board)
   end
 end
+
+class Something
+  def initialize
+    @something = []
+  end
+
+  def dosomething
+    s = 5
+    @something << s
+  end
